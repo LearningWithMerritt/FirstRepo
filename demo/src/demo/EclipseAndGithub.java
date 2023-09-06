691e038b7f5ba715f2b952b08f5165216e331405
LWM <learningwithmerritt@gmail.com>
2023-09-05 23:45:36 -0700

    "Updated with Eclipse"

---
+++ demo/src/demo/EclipseAndGithub.java
@@ -0,0 +1,16 @@
+
+public class EclipseAndGithub {
+
+    private String a = "Downloading and install Eclipse";
+    private String b = "Importing a Config File";
+    private String c = "Creating a Workspace";
+    private String d = "Default Workspace Location";
+    private String e = "Setting up your own Workspace";
+    private String f = "Creating a Project";
+    private String g = "Creating a new Class";
+    private String h = "Importing a Java File";
+    private String i = "Setting Up Eclipse with Github";
+
+
+
+}
